4028b1096aa5911a59016aa3b131e5080d381f5e
That1GuyWSSC <33987876+That1GuyWSSC@users.noreply.github.com>
2022-01-19 23:52:10 +0000

    inicio do repositorio

---
+++ structs.h
@@ -0,0 +1,16 @@
+typedef struct 
+{
+    int ordem;
+    int nif;
+    char codigo[100];
+    int tempo;
+    int distancia;
+}ME;
+
+typedef struct 
+{
+    char codigo[100];
+    char tipo[100];
+    float custo;
+    int autonomia;
+}MU;
